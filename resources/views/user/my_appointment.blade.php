9ea1709b44d6c911b627fdc3ed7f4438987fb0c1
Asmae Laaroussi <asmaelaaroussi221@gmail.com>
2025-01-10 02:46:25 +0100

    modifying the code

--- resources/views/user/my_appointment.blade.php
+++ resources/views/user/my_appointment.blade.php
@@ -38,13 +38,13 @@
          <div class="header_main">
             <div class="mobile_menu">
                <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                  <div class="logo_mobile"><a href="index.html"><img src="images/logo.png"></a></div>
+                  <div class="logo_mobile"><a href="index.html"><img src="images/logoT.png"></a></div>
                   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                   <span class="navbar-toggler-icon"></span>
                   </button>
                   <div class="collapse navbar-collapse" id="navbarNav">
                      <ul class="navbar-nav">
-                        <li class="nav-item"><a class="nav-link" href="index.html">Home</a></li>
+                        <li class="nav-item"><a class="nav-link" href="/">Home</a></li>
                         <li class="nav-item"><a class="nav-link" href="about.html">About</a></li>
                         <li class="nav-item"><a class="nav-link " href="contact.html">Contact</a></li>
                         <li class="nav-item"><a class="nav-link" href="{{ route('login') }}">Login</a></li>
@@ -54,12 +54,12 @@
                </nav>
             </div>
             <div class="container-fluid">
-               <div class="logo"><a href="index.html"><img src="images/logo.png"></a></div>
+               <div class="logo"><a href="index.html"><img src="images/logoT.png"></a></div>
                <div class="menu_main">
                   <ul>
-                     <li class="active"><a href="index.html">Home</a></li>
-                     <li><a href="about.html">About</a></li>
-                     <li><a href="contact.html">Contact us</a></li>
+                     <li class="active"><a href="/">Home</a></li>
+                     <li><a href="/">About</a></li>
+                     <li><a href="/">Contact us</a></li>
                      @if(Route::has('login'))
                         @auth
                         <li><a href="{{url('myappointment')}}">My Appointments</a></li>
